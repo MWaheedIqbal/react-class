290eb170080c393cf675e3c402c857fd01d0ba9b
Muhammad Waheed Iqbal <tesla.dxb@gmail.com>
2023-05-07 16:34:00 +0400

    React Basics

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Avatar from "./components/Avatar";
+import Gallery from "./components/Gallery";
+// import { Profile } from "./components/Gallery";
+import Profile from "./components/Profile";
+import ToDo from "./components/ToDo";
+import TodoList from "./components/ToDoList";
+
+export default function Home() {
+  return (
+  <div>
+    <Profile person={{ name: "Lin Lanying", imageId: "1bX5QH6" }}
+    size={200} />
+  </div>)
+}
